aa9400ceacf6567c55fd7425cb984fbaa9d4d82b
Bill Huang <bill.huang2001@gmail.com>
2026-03-03 16:21:22 +0800

    Update checksum

--- checksum-Elixir.ExGix.Native.exs
+++ checksum-Elixir.ExGix.Native.exs
@@ -1,8 +1,8 @@
 %{
-  "ex_gix-v0.1.0-nif-2.15-x86_64-pc-windows-gnu.dll.tar.gz" => "sha256:67ee8971083be6a7edbd9fb22a36730c9cdb77cfbb8b918482848a9ac13d0360",
-  "ex_gix-v0.1.0-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:f60e5af6c38ba38227e4f222aecc9a00f779a6fb1c8b23c5b8d8f6f6235ccef6",
-  "libex_gix-v0.1.0-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:05642720caab26ff663e39ef250351fd38e6e2410b3ba3e5b81e18e85e2a211a",
-  "libex_gix-v0.1.0-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:fffaec2192d65f005a72d2bc4f784084c1f6110c0e764f50f3fbf818cd774d42",
-  "libex_gix-v0.1.0-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:f739a5f0c71f1a92c6e46852c6d17c3807dfa5bc7b59bf9aa9326c1c3f3e755c",
-  "libex_gix-v0.1.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:a9360964411e20e9a3e2b4e7896479e8553c06e8decfe5f5126ba60b237c167d",
+  "ex_gix-v0.1.1-nif-2.15-x86_64-pc-windows-gnu.dll.tar.gz" => "sha256:e8f8e79e6d8b3bd9add885e87f765e106072a13d0fc93bc7cbf194fe6cc4b854",
+  "ex_gix-v0.1.1-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:163579cc17ecd5154d8a9578deeb9ef2be4bf51e9f5d9c40c936f105765ac474",
+  "libex_gix-v0.1.1-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:e7a5369467e5e3562dc5971b2e3d3e8bac3856569453d34ef3bff34947a91d05",
+  "libex_gix-v0.1.1-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:30733f562a66c19fbb32986ef942e77e7a44d37d95eba58b74afb2fe42a3d2e2",
+  "libex_gix-v0.1.1-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:35fd2c19124f4881e19c528f24895beab4687b04b1b11fae9253ec211cbd198d",
+  "libex_gix-v0.1.1-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:5c1152d3b47c75d38ba730d388d32cd955bc9b36b58ccec53532efa074086a63",
 }
